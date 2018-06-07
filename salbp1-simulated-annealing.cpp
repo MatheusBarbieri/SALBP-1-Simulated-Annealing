8bc8e5b13014c21a1521b557ba8c60c4d6da7328
Alexandre Leuck <aleuck2004@gmail.com>
2018-06-07 02:28:31 -0300

    instance from stdin and options from args

--- salbp1-simulated-annealing.cpp
+++ salbp1-simulated-annealing.cpp
@@ -4,16 +4,22 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
-#include <utility> #include <vector>
+#include <utility>
+#include <vector>
+#include <unistd.h>
 
 class Solution;
 
-int numberOfTasks, cycleTime, iterations, seed;
-double temperature, decay, temperatureLimit;
+int numberOfTasks;
 std::vector<int> taskTimes;
 std::vector<std::vector <int>> precedenceOrder;
-std::string inputFileName;
 
+int cycleTime = 6;
+double temperature = 1;
+double temperatureLimit = 0.00001;
+double decay = 0.9;
+int iterations = 100;
+int seed = 0;
 
 class Solution{
 private:
@@ -100,46 +106,84 @@ long Solution::getValue() const {
     return value;
 }
 
-void readFile(std::string &fileName){
-    std::ifstream file;
-    file.open(fileName);
+void readInstance() {
     int currentValue;
-    file >> numberOfTasks;
+    std::cin >> numberOfTasks;
     for(int i=0; i<numberOfTasks; i++){
-        file >> currentValue;
+        std::cin >> currentValue;
         taskTimes.push_back(currentValue);
     }
-    while(file >> currentValue){
+    while(std::cin >> currentValue){
         if (currentValue == -1){
             break;
         }
         int predecessor = currentValue;
         int successor;
-        file.ignore(1);
-        file >> successor;
+        std::cin.ignore(1);
+        std::cin >> successor;
         std::vector<int> precedence = {predecessor-1, successor-1};
         precedenceOrder.push_back(precedence);
     }
-    file.close();
+}
+
+void printInstance() {
+    std::cout << "tasks: " << numberOfTasks << std::endl;
+
 }
 
 int main(int argc, char **argv) {
-    if (argc < 8){
-        std::cout << "Incorret Usage! Correct Usage:\n\t"
-                     "./salbp1-sa <inputfile> <cycle-time> <temperature> <decay>"
-                     " <iterations-before-decay> <limit-temperature> <seed>" << std::endl;
-        return -1;
-    } else {
-        inputFileName = argv[1];
-        cycleTime = atoi(argv[2]);
-        temperature = atof(argv[3]);
-        decay = atof(argv[4]);
-        iterations = atoi(argv[5]);
-        temperatureLimit = atof(argv[6]);
-        seed = atoi(argv[7]);
+    int opt;
+    while ((opt = getopt(argc, argv, "c:t:d:i:l:s:")) != -1) {
+        switch (opt) {
+            case 'c':
+                cycleTime = atoi(optarg);
+                break;
+            case 't':
+                temperature = atof(optarg);
+                break;
+            case 'l':
+                temperatureLimit = atof(optarg);
+                break;
+            case 'd':
+                decay = atof(optarg);
+                break;
+            case 'i':
+                iterations = atoi(optarg);
+                break;
+            case 's':
+                seed = atoi(optarg);
+                break;
+            default:
+                std::cout << "Usage:" << argv[0] << "[OPTIONS]...\n";
+                std::cout << "\tOPTIONS:\n";
+                std::cout << "\t\t-c cycle_time\n";
+                std::cout << "\t\t-t temperature\n";
+                std::cout << "\t\t-l temperature_limit\n";
+                std::cout << "\t\t-d temperature_decay\n";
+                std::cout << "\t\t-i iterations\n";
+                std::cout << "\t\t-s seed\n";
+                exit(EXIT_FAILURE);
+                break;
+
+        }
     }
 
-    readFile(inputFileName);
+    if (seed == 0) {
+        seed = rand();
+    }
+    srand(seed);
+    std::cout << "command to reproduce:" << std::endl;
+    std::cout << argv[0];
+    std::cout << " -c " << cycleTime;
+    std::cout << " -t " << temperature;
+    std::cout << " -l " << temperatureLimit;
+    std::cout << " -d " << decay;
+    std::cout << " -i " << iterations;
+    std::cout << " -s " << seed;
+    std::cout << "\n\n";
+    std::cout << "Random seed: " << seed << std::endl;
+
+    readInstance();
     srand((u_int) seed);
 
     Solution currentSolution;
